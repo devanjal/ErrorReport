b744fe3127d02b67ee426b353ded5705b77ea443
devanjal <devanjal_kotia@yahoo.co.in>
2018-04-09 14:10:17 -0700

    New LoggerUtil

--- src/main/java/com/demo/error/LoggerUtil.java
+++ src/main/java/com/demo/error/LoggerUtil.java
@@ -1,36 +1,39 @@
 package com.demo.error;
+
 import com.google.cloud.MonitoredResource;
+import com.google.api.gax.paging.Page;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.Logging;
+import com.google.cloud.logging.Logging.EntryListOption;
 import com.google.cloud.logging.LoggingOptions;
 import com.google.cloud.logging.Payload.StringPayload;
-import com.google.cloud.logging.Severity;
 
 
 
 
 import java.util.Collections;
 public class LoggerUtil {
-	 public static void main(String... args) throws Exception {
+	public static void main(String... args) throws Exception {
+	    // Create a service object
+	    // Credentials are inferred from the environment
+	    LoggingOptions options = LoggingOptions.getDefaultInstance();
+	    try(Logging logging = options.getService()) {
 
-		    // Instantiates a client
-		    Logging logging = LoggingOptions.getDefaultInstance().getService();
+	      // Create a log entry
+	      LogEntry firstEntry = LogEntry.newBuilder(StringPayload.of("message"))
+	          .setLogName("test-log")
+	          .setResource(MonitoredResource.newBuilder("global")
+	              .addLabel("project_id", options.getProjectId())
+	              .build())
+	          .build();
+	      logging.write(Collections.singleton(firstEntry));
 
-		    // The name of the log to write to
-		    String logName = "Dev-Log";  // "my-log";
-
-		    // The data to write to the log
-		    String text = "Hello, world!";
-
-		    LogEntry entry = LogEntry.newBuilder(StringPayload.of(text))
-		        .setSeverity(Severity.ERROR)
-		        .setLogName(logName)
-		        .setResource(MonitoredResource.newBuilder("global").build())
-		        .build();
-
-		    // Writes the log entry asynchronously
-		    logging.write(Collections.singleton(entry));
-
-		    System.out.printf("Logged: %s%n", text);
-		  }
+	      // List log entries
+	      Page<LogEntry> entries = logging.listLogEntries(
+	          EntryListOption.filter("logName=projects/" + options.getProjectId() + "/logs/test-log"));
+	      for (LogEntry logEntry : entries.iterateAll()) {
+	        System.out.println(logEntry);
+	      }
+	    }
+	  }
 }
