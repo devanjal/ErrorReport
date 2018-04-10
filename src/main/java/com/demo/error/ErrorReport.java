4d8bd2eb73a75eecb8f9a6a7560c6024c0f27e01
Devanjal Kotia <devanjal.kotia@danalcorp.com>
2018-04-10 14:03:39 -0700

    Final setting up of loggerUtil

--- src/main/java/com/demo/error/ErrorReport.java
+++ src/main/java/com/demo/error/ErrorReport.java
@@ -2,17 +2,27 @@ package com.demo.error;
 
 import java.io.IOException;
 
+import com.google.cloud.logging.Severity;
+
 public class ErrorReport {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		ReportUtil report = new ReportUtil();
+		
+		LoggerUtil logger= LoggerUtil.getInstance();
+		
 		String className=ErrorReport.class.getName();
 		String methodName=Thread.currentThread().getStackTrace()[1].getMethodName();
 		String errorMessage="Test Code";
 		
-		report.report(className,Thread.currentThread().getStackTrace()[1].
-			      getLineNumber(), methodName, errorMessage);
+/*		report.report(className,Thread.currentThread().getStackTrace()[1].
+			      getLineNumber(), methodName, errorMessage);*/
+		
+		logger.log(Severity.ERROR, className, "ERROR part");
+		logger.log(Severity.CRITICAL, className, "CRITICAL part");
+		logger.log(Severity.WARNING, className, "WARNING part");
+		logger.log(Severity.ALERT, className, "ALERT part");
 	}
 
 }
--- src/main/java/com/demo/error/LoggerUtil.java
+++ src/main/java/com/demo/error/LoggerUtil.java
@@ -1,6 +1,5 @@
 package com.demo.error;
 
-
 import com.google.cloud.MonitoredResource;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.Logging;
@@ -8,31 +7,33 @@ import com.google.cloud.logging.LoggingOptions;
 import com.google.cloud.logging.Payload.StringPayload;
 import com.google.cloud.logging.Severity;
 
+
 import java.util.Collections;
 public class LoggerUtil {
+	
+	 private static LoggerUtil instance = null;
+	   protected LoggerUtil() {
+		   
+	   }
+	   public static LoggerUtil getInstance() {
+	      if(instance == null) {
+	         instance = new LoggerUtil();
+	      }
+	      return instance;
+	   }
 
-		public static void main(String args[]) throws Exception {
+		public void log(Severity level, String appName, String message) throws Exception {
 			
 		    Logging logging = LoggingOptions.getDefaultInstance().getService();
 
-		    String logName = "test_logs_danal";
 
-		    String text = "Danal StackDriver Testing";
-
-		    LogEntry entry = LogEntry.newBuilder(StringPayload.of(text + "ERROR"))
-		        .setSeverity(Severity.ERROR)
-		        .setLogName(logName)
-		        .setResource(MonitoredResource.newBuilder("global").build())
+		    LogEntry entry = LogEntry.newBuilder(StringPayload.of(message + "ERROR"))
+		        .setSeverity(level)
+		        .setLogName(appName)
+		        .setResource(MonitoredResource.newBuilder("global").setType("gce_instance").build())
 		        .build();
-		    LogEntry entry2 = LogEntry.newBuilder(StringPayload.of(text + "INFO"))
-			        .setSeverity(Severity.INFO)
-			        .setLogName(logName)
-			        .setResource(MonitoredResource.newBuilder("global").setType("gce_instance").build())
-			        .build();
-
 		    logging.write(Collections.singleton(entry));
-		    logging.write(Collections.singleton(entry2));
 
-		    System.out.printf("Logged: %s%n", text);
+		    System.out.printf("Logged: %s%n", message);
 		  }
 }
