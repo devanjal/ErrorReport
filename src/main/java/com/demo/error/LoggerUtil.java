beaffbe23b81c3f8fa1cb86dc88d90d2d1536cfe
Devanjal Kotia <devanjal.kotia@danalcorp.com>
2018-04-16 15:24:58 -0700

    try 13

--- src/main/java/com/demo/error/LoggerUtil.java
+++ src/main/java/com/demo/error/LoggerUtil.java
@@ -1,6 +1,7 @@
 package com.demo.error;
 
 
+import com.google.api.client.util.Value;
 import com.google.cloud.MetadataConfig;
 import com.google.cloud.MonitoredResource;
 import com.google.cloud.logging.LogEntry;
@@ -8,7 +9,7 @@ import com.google.cloud.logging.Logging;
 import com.google.cloud.logging.LoggingOptions;
 import com.google.cloud.logging.Payload.StringPayload;
 import com.google.cloud.logging.Severity;
-import com.google.rpc.ResourceInfo;
+import com.google.cloud.MonitoredResourceDescriptor;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -39,9 +40,8 @@ public class LoggerUtil {
 			label.put("instance_id", instanceId);
 			label.put("project_id", projectId);
 			label.put("zone", instanceZone);
-			
 		    Logging logging = LoggingOptions.getDefaultInstance().getService();
-		   System.out.println("**************"+ResourceInfo.getDescriptor().getFullName().toString());
+		   System.out.println("**************"+MetadataConfig.getInstanceId());
 		   
 		    
 		    LogEntry entry = LogEntry.newBuilder(StringPayload.of(message))
